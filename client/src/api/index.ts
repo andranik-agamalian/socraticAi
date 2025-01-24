9962fdc27f250c4e2af985dfe1a2781fd1a34353
Dean Ohashi <dean@swecrets.com>
2025-01-23 19:07:13 -0800

    feat: Add basic API abstraction

---
+++ client/src/api/index.ts
@@ -0,0 +1,15 @@
+
+import { Conversation } from "../types";
+
+const BASE_URL = "http://localhost:3000/api/v1";
+
+const headers = new Headers();
+headers.append("Content-Type", "application/json");
+
+export const chat = (message: string, conversationHistory: Conversation[]) => {
+  return fetch(`${BASE_URL}/chat`, {
+    method: "POST",
+    body: JSON.stringify({ message, conversationHistory }),
+    headers,
+  });
+}
