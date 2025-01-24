61968ff8184bdfec0802ac618f8d22be529a1ada
Dean Ohashi <dean@swecrets.com>
2025-01-23 19:08:28 -0800

    feat: Add basic abstraction for openAI related state and methods

---
+++ client/src/hooks/useOpenAI.ts
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { chat } from "../api";
+
+export const useOpenAI = () => {
+  const [conversationHistory, setConversationHistory] = useState([]);
+
+  const send = async (message: string) => {
+      const response = await chat(message, conversationHistory);
+  
+      // TODO: Handle error if response.status !== 200
+      const data = await response.json();
+
+      setConversationHistory(data.conversationHistory);
+  }
+
+  return { conversationHistory, send };
+}
